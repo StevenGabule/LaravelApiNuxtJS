dd55432aab14a2149637d0602208032dd253e836
John Paul Lim Gabule <lucas.gabule@gmail.com>
2020-03-24 09:55:59 +0800

    laravel api login user

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@
 Route::group(['middleware' => ['guest:api']], static function() {
     Route::post('register', 'Auth\RegisterController@register');
     Route::post('verification/verify/{user}', 'Auth\VerificationController@verify')->name('verification.verify');
-    Route::post('verification/resend', 'Auth\VerificationController@resend');
+    Route::post('login', 'Auth\LoginController@login');
 //    Route::post('verification/resend', 'Auth\VerificationController@resend');
 });
 
